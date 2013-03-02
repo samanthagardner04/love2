c27055774f499c042e7e951b8e09b3a20286ea4b
Colin Kuhn <colin.kuhn2@gmail.com>
2013-03-02 15:21:38 -0600

    Added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,58 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Item.destroy_all
+
+categories = ["pillow", "bag", "apron"]
+
+20.times do
+
+  randomizer = [0,1].shuffle
+  i = Item.new
+  i.price = rand(1..300)
+  i.user = User.first
+  i.story = "Random Story Random Story Random Story"
+  i.name= "Sample Item Name"
+  i.save
+end
+
+puts "Created 20 items"
