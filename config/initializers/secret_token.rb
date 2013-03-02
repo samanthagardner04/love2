7be99669316d4d4d39a183927ddf18736ff79d8b
Colin Kuhn <colin.kuhn2@gmail.com>
2013-03-02 11:58:02 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Love2::Application.config.secret_token = 'ff9170f28a3275e5be30f3dae0bd7d1754447564995ca5129f24089ef54fd63ce37b12fa56d30ffe06b53f608b3be1e98fb54400613106b4c1494627bd813ae6'
