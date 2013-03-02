6ecf7788e3315cba05c9f36903af7c4abe8add5f
Ty Diamse <tylerdiamse@gmail.com>
2013-03-02 13:34:56 -0600

    added the boolean to true

--- db/schema.rb
+++ db/schema.rb
@@ -34,9 +34,9 @@ ActiveRecord::Schema.define(:version => 20130302180539) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.boolean  "admin"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.boolean  "admin",           :default => false
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
     t.string   "password_digest"
   end
 
