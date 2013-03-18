e8feb3c0d4c3120e9027041c70a1966f270f84c5
Colin Kuhn <colin.kuhn2@gmail.com>
2013-03-18 16:59:25 -0500

    added bootswatch and improved seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,18 +17,33 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-Item.destroy_all
+
+1.times do
+  u=User.new
+  u.name="Johnny Test"
+  u.email="Email@test.com"
+  u.password="Password"
+  u.password_confirmation="Password"
+  u.save
+end
 
 
+3.times do
+  c=Category.new
+  c.name="Pillows"
+  c.description="Category Description"
+  c.save
+end
 
-10.times do
+4.times do
   i = Item.new
   i.price = rand(1..300)
   i.user = User.first
   i.story = "Random Story"
+  i.description="Description Description"
   i.name= "Sample Item Name"
   i.category= Category.first
   i.save
 end
 
-puts "deleted items"
+puts "seeds file finished"
